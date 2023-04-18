a0657d03c3b8fcb822756b99f5a7f1a16c3b2aba
Hemna <waboring@hemna.com>
2023-04-18 11:45:00 -0400

    Initial commit

---
+++ aprsd_wxnow_plugin/aprsd_wxnow_plugin.py
@@ -0,0 +1,68 @@
+import logging
+
+from aprsd import packets, plugin
+from aprsd.utils import trace
+from oslo_config import cfg
+
+import aprsd_wxnow_plugin
+from aprsd_wxnow_plugin import conf  # noqa
+
+
+CONF = cfg.CONF
+LOG = logging.getLogger("APRSD")
+
+
+class WXNowPlugin(plugin.APRSDRegexCommandPluginBase):
+
+    version = aprsd_wxnow_plugin.__version__
+    # Change this regex to match for your plugin's command
+    # Tutorial on regex here: https://regexone.com/
+    # Look for any command that starts with w or W
+    command_regex = "^[wW]"
+    # the command is for ?
+    # Change this value to a 1 word description of the plugin
+    # this string is used for help
+    command_name = "weather"
+
+    enabled = False
+
+    def setup(self):
+        """Allows the plugin to do some 'setup' type checks in here.
+
+        If the setup checks fail, set the self.enabled = False.  This
+        will prevent the plugin from being called when packets are
+        received."""
+        # Do some checks here?
+        self.enabled = True
+
+    def create_threads(self):
+        """This allows you to create and return a custom APRSDThread object.
+
+        Create a child of the aprsd.threads.APRSDThread object and return it
+        It will automatically get started.
+
+        You can see an example of one here:
+        https://github.com/craigerl/aprsd/blob/master/aprsd/threads.py#L141
+        """
+        if self.enabled:
+            # You can create a background APRSDThread object here
+            # Just return it for example:
+            # https://github.com/hemna/aprsd-weewx-plugin/blob/master/aprsd_weewx_plugin/aprsd_weewx_plugin.py#L42-L50
+            #
+            return []
+
+    @trace.trace
+    def process(self, packet: packets.core.Packet):
+
+        """This is called when a received packet matches self.command_regex.
+
+        This is only called when self.enabled = True and the command_regex
+        matches in the contents of the packet["message_text"]."""
+
+        LOG.info("WXNowPlugin Plugin")
+
+        packet.from_call
+        packet.message_text
+
+        # Now we can process
+        return "some reply message"
